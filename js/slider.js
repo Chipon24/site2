8e46a326df8df6cdf01670050d2fa9c3afd36436
Chipon24 <91572533+Chipon24@users.noreply.github.com>
2022-06-08 23:36:23 +0300

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+$('.single-item').slick({
+  infinite: true,
+  arrows: false, // не показывать стрелки
+  dots: true,
+  dotsClass: 'slick-dots',
+  autoplay: true,
+  autoplaySpeed: 2000,
+  // fade: true,
+	// fadeSpeed: 1000
+});
